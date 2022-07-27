2cc084e9a730cfb797a711e2d4bbc124d1cca46a
Bertrand <bertrand.augereau@gmail.com>
2022-07-27 21:46:55 +0200

    Debug prints

--- Network.gd
+++ Network.gd
@@ -18,16 +18,19 @@ func _ready() -> void:
 	for ip in IP.get_local_addresses():
 		if ip.begins_with("169.254") and not ip.ends_with(".1"):
 			ip_address = ip
-			
+	
+	print("ip_address = ", ip_address)		
 	get_tree().connect("connected_to_server", self, "_connected_to_server")
 	get_tree().connect("server_disconnected", self, "_server_disconnected")
 
 func create_server() -> void:
+	print("create_server")
 	server = NetworkedMultiplayerENet.new()
-	server. create_server(DEFAULT_PORT, MAX_CLIENTS)
+	server.create_server(DEFAULT_PORT, MAX_CLIENTS)
 	get_tree().set_network_peer(server)
 
 func join_server() -> void:
+	print("join_server")
 	client = NetworkedMultiplayerENet.new()
 	client.create_client(ip_address, DEFAULT_PORT)
 	get_tree().set_network_peer(client)
