7d436e59c2bd12caef89e793fe399449dd0b98f9
Lucas Gerretsen <lucas.gerretsen2@gmail.com>
2022-07-29 13:40:06 +0200

    Merge with https://github.com/lgerrets/tsuri 94258333b282ac2318b0de19726709673a648bfa [wip] Support 2 players [add] Print all local addresses [add] Font

--- Network.gd
+++ Network.gd
@@ -1,27 +1,36 @@
 extends Node
 
 const DEFAULT_PORT = 28960
-const MAX_CLIENTS = 2
+const MAX_CLIENTS = 6
 
 var server = null
 var client = null
 
 var ip_address = ""
 
-# Called when the node enters the scene tree for the first time.
+#var client_connected_to_server = false
+
+#var networked_object_name_index = 0 setget networked_object_name_index_set
+#puppet var puppet_networked_object_name_index = 0 setget puppet_networked_object_name_index_set
+
+#onready var client_connection_timeout_timer = Timer.new()
+
 func _ready() -> void:
-	if OS.get_name() == "Windows":
-		ip_address = IP.get_local_addresses()[3]
-	else:
-		ip_address = IP.get_local_addresses()[3]
-	
-	for ip in IP.get_local_addresses():
-		if ip.begins_with("169.254") and not ip.ends_with(".1"):
-			ip_address = ip
+#	add_child(client_connection_timeout_timer)
+#	client_connection_timeout_timer.wait_time = 10
+#	client_connection_timeout_timer.one_shot = true
+#
+#	client_connection_timeout_timer.connect("timeout", self, "_client_connection_timeout")
+
+	var local_adresses = IP.get_local_addresses()
+	for ip in local_adresses:
+		if (ip.find(":") == -1) and not (ip.begins_with("192.168.") and ip.ends_with(".1")): # let's not print ipv6 ips, nor virtual wifi ip
+			ip_address = ip_address + ip + "\n"
 	
 	print("ip_address = ", ip_address)		
 	get_tree().connect("connected_to_server", self, "_connected_to_server")
 	get_tree().connect("server_disconnected", self, "_server_disconnected")
+#	get_tree().connect("connection_failed", self, "_connection_failed")
 
 func create_server() -> void:
 	print("create_server")
@@ -30,6 +39,7 @@ func create_server() -> void:
 	if error != 0:
 		print("error = ", error)
 	get_tree().set_network_peer(server)
+#	Global.instance_node(load("res://Server_advertiser.tscn"), get_tree().current_scene)
 
 func join_server() -> void:
 	print("join_server")
@@ -38,13 +48,55 @@ func join_server() -> void:
 	if error != 0:
 		print("error = ", error)
 	get_tree().set_network_peer(client)
+#	client_connection_timeout_timer.start()
+
+#func reset_network_connection() -> void:
+#	if get_tree().has_network_peer():
+#		get_tree().network_peer = null
+
+#func _connected_to_server() -> void:
+#	print("Successfully connected to the server")
 	
-func _connected_to_server() -> void:
-	print("Successfully connected to the server")
-	
+#	client_connected_to_server = true
+
 func _server_disconnected() -> void:
 	print("Disconnected from the server")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	
+#	for child in Persistent_nodes.get_children():
+#		if child.is_in_group("Net"):
+#			child.queue_free()
+#
+#	reset_network_connection()
+#
+#	if Global.ui != null:
+#		var prompt = Global.instance_node(load("res://Simple_prompt.tscn"), Global.ui)
+#		prompt.set_text("Disconnected from server")
+#
+#func _client_connection_timeout():
+#	if client_connected_to_server == false:
+#		print("Client has been timed out")
+#
+#		reset_network_connection()
+#
+#		var connection_timeout_prompt = Global.instance_node(load("res://Simple_prompt.tscn"), get_tree().current_scene)
+#		connection_timeout_prompt.set_text("Connection timed out")
+#
+#func _connection_failed():
+#	for child in Persistent_nodes.get_children():
+#		if child.is_in_group("Net"):
+#			child.queue_free()
+#
+#	reset_network_connection()
+#
+#	if Global.ui != null:
+#		var prompt = Global.instance_node(load("res://Simple_prompt.tscn"), Global.ui)
+#		prompt.set_text("Connection failed")
+#
+#func puppet_networked_object_name_index_set(new_value):
+#	networked_object_name_index = new_value
+#
+#func networked_object_name_index_set(new_value):
+#	networked_object_name_index = new_value
+#
+#	if get_tree().is_network_server():
+#		rset("puppet_networked_object_name_index", networked_object_name_index)
