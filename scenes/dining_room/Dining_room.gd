27af8fba14e6f153cc4ef829b07a1a4ae3ce3b30
fmdkdd <fmdkdd@0xc0de.fr>
2022-07-30 12:18:58 +0200

    Order composition follows order of word click

--- scenes/dining_room/Dining_room.gd
+++ scenes/dining_room/Dining_room.gd
@@ -10,7 +10,9 @@ func _ready():
 	$SendOrder.connect("pressed", self, "_on_SendOrder_pressed")
 	$SendOrder.disabled = true
 
-	$WordList.connect("multi_selected", self, "_on_WordList_multi_selected")
+	$ClearOrder.connect("pressed", self, "_on_ClearOrder_pressed")
+
+	$WordList.connect("item_selected", self, "_on_WordList_item_selected")
 	$OrderPreview.text = ""
 
 	waiter = waiter_scene.instance()
@@ -40,31 +42,25 @@ func _process(delta):
 func _on_CheffeDish_Sent(dish):
 	print("cheffe dish ", dish)
 
-func get_current_order() -> Array:
-	var order_words = []
-	for it in $WordList.get_selected_items():
-		order_words.append($WordList.get_item_text(it))
-
-	return order_words
-
-func build_order_string(order_words) -> String:
-	var order = ""
-	for w in order_words:
-		order += w + " "
-	return order
+func get_current_order() -> String:
+	return $OrderPreview.text.strip_edges()
 
 func send_order(order):
 	print("sent order ", order)
 	Global.waiter_send_command(order)
 
-func _on_WordList_multi_selected(index: int, selected: bool):
-	var current_order = get_current_order()
-	$OrderPreview.text = build_order_string(current_order)
-	$SendOrder.disabled = current_order.size() == 0
+func _on_WordList_item_selected(index: int):
+	var word = $WordList.get_item_text(index)
+	$OrderPreview.text += " " + word
+	$WordList.unselect_all()
+	$SendOrder.disabled = false
 
 func _on_SendOrder_pressed():
 	var order = get_current_order()
 	send_order(order)
-	$WordList.unselect_all()
 	$SendOrder.disabled = true
 	$OrderPreview.text = ""
+
+func _on_ClearOrder_pressed():
+	$OrderPreview.text = ""
+	$SendOrder.disabled = true
