283231d1e282580c33dea78b32bf1da6e042149d
Lucas Gerretsen <lucas.gerretsen2@gmail.com>
2022-07-30 12:40:22 +0200

    the sent dish string is the concatenation of checked boxes

--- scenes/kitchen/Cheffe.gd
+++ scenes/kitchen/Cheffe.gd
@@ -1,14 +1,7 @@
 extends KinematicBody2D
 
-onready var send_dish = $SendDish
-onready var dish_input = $DishInput
-
 func _ready():
-	send_dish.connect("pressed", self, "_on_ButtonPressed")
+	pass
 
 func _process(delta: float) -> void:
 	pass
-
-func _on_ButtonPressed():
-	var dish = dish_input.text
-	Global.cheffe_send_dish(dish)
