7bc2a44ffed6bf84059907e45cd470ec541ca1f9
fmdkdd <fmdkdd@0xc0de.fr>
2022-07-30 10:27:43 +0200

    Click patrons to show order

--- scenes/dining_room/Dining_room.gd
+++ scenes/dining_room/Dining_room.gd
@@ -4,15 +4,13 @@ var waiter_scene = preload("res://scenes/dining_room/Waiter.tscn")
 
 var waiter
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.connect("cheffe_dish_sent", self, "_on_CheffeDish_Sent")
 	waiter = waiter_scene.instance()
 	add_child(waiter)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
 
 func _on_CheffeDish_Sent(dish):
 	print("cheffe dish ", dish)
---
+++ scenes/dining_room/Patron.gd
@@ -0,0 +1,66 @@
+extends KinematicBody2D
+
+var food_assets = []
+
+enum State {
+#	ENTERING,
+	WAITING_TO_ORDER,
+	WAITING_TO_EAT,
+	EATING,
+#	LEAVING,
+}
+
+var state
+var wanted_dish
+var in_current_state_since = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var food_files = [
+		"res://assets/food/black_forest_hole.png",
+		"res://assets/food/blue_banana.png",
+		"res://assets/food/blue_banana.png",
+		"res://assets/food/ghosts.png",
+		"res://assets/food/springs.png",
+	]
+
+	for f in food_files:
+		food_assets.append(load(f))
+
+	state = State.WAITING_TO_ORDER
+	hide_wanted_dish()
+	wanted_dish = generate_dish()
+
+func _process(delta):
+	pass
+
+func _on_Patron_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		_on_Patron_pressed()
+
+func _on_Patron_pressed():
+	if state == State.WAITING_TO_ORDER:
+		toggle_wanted_dish()
+	elif state == State.WAITING_TO_EAT:
+		pass # TODO
+	elif state == State.EATING:
+		pass # nothing to be done
+	else:
+		assert(false)
+
+
+func toggle_wanted_dish():
+	if $DishWish.visible:
+		hide_wanted_dish()
+	else:
+		show_wanted_dish()
+
+func show_wanted_dish():
+	$DishWish.show()
+
+func hide_wanted_dish():
+	$DishWish.hide()
+
+func generate_dish():
+	var food_img = food_assets[randi() % food_assets.size()]
+	$DishWish/Sprite.texture = food_img
