7f329a36b3d5fa9e94bd6d4927d552848b3fe9fc
fmdkdd <fmdkdd@0xc0de.fr>
2022-07-30 10:27:43 +0200

    Click patrons to show order

--- scenes/dining_room/Waiter.gd
+++ scenes/dining_room/Waiter.gd
@@ -1,19 +1,7 @@
 extends KinematicBody2D
 
-onready var send_command = $SendCommand
-onready var command_input = $CommandInput
-
 func _ready():
-	send_command.connect("pressed", self, "_on_ButtonPressed")
+	pass
 
 func _process(delta: float) -> void:
 	pass
-
-func _on_ButtonPressed():
-	var command = command_input.text
-	Global.waiter_send_command(command)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
