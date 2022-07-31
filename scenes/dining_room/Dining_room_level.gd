9840603ef14969b387f47a9631058de6203caa64
Lucas Gerretsen <lucas.gerretsen2@gmail.com>
2022-07-31 13:40:29 +0200

    wip navigation : everything should be in place, debugging

--- scenes/dining_room/Dining_room_level.gd
+++ scenes/dining_room/Dining_room_level.gd
@@ -5,11 +5,7 @@ onready var layout = $Layout
 onready var dining_room = $DiningRoom
 onready var waiter = $Layout/Waiter
 
-onready var patron_dummy = $Layout/Patron # TODO; these should be spawned at runtime
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var patron_dummy = $Patrons/Patron # TODO; these should be spawned at runtime
 
 
 # Called when the node enters the scene tree for the first time.
@@ -31,24 +27,31 @@ func _on_CheffeDish_Sent(dish):
 
 func _set_layout_visible(_visible):
 	layout.visible = _visible
-	# TODO: we may also need to enable/disable controls
+	# TODO: we may also need to enable/disable controls eg pickable_inputs
+	for patron in $Patrons.get_children():
+		patron.get_node("LevelAvatar").visible = _visible
+		patron.get_node("LevelAvatar").input_pickable = _visible
+		patron.get_node("CommandAvatar").visible = not _visible
+		patron.get_node("CommandAvatar").input_pickable = not _visible
 	
 func _on_Patron_clicked(patron):
-	match patron.state:
-		patron.State.WAITING_TO_ORDER:
-			if (patron.position.distance_to(waiter.position) < 50) and (dining_room.state == dining_room.State.NOT_VISIBLE):
-				dining_room.pop_up([patron])
-				_set_layout_visible(false)
-		patron.State.ORDERING:
-			if tray.selected_dish != null:
-				var dish = tray.selected_dish
-				tray.remove_dish(dish)
-				patron.serve_dish(dish)
-			else:
-				patron.toggle_wanted_dish()
-		patron.State.SHOW_DISH_SCORE:
-			patron.hide_dish_score()
-			patron.set_state(patron.State.LEAVING)
+	if (patron.get_node("LevelAvatar").global_position.distance_to(waiter.position) < 80) and (dining_room.state == dining_room.State.NOT_VISIBLE):
+		dining_room.pop_up([patron])
+		_set_layout_visible(false)
+	if dining_room.state == dining_room.State.VISIBLE:
+		match patron.state:
+			patron.State.WAITING_TO_ORDER:
+				patron.set_state(patron.State.ORDERING)
+			patron.State.ORDERING:
+				if tray.selected_dish != null:
+					var dish = tray.selected_dish
+					tray.remove_dish(dish)
+					patron.serve_dish(dish)
+				else:
+					patron.toggle_wanted_dish()
+			patron.State.SHOW_DISH_SCORE:
+				patron.hide_dish_score()
+				patron.set_state(patron.State.LEAVING)
 
 func on_close_command_popup():
 	assert(dining_room.state == dining_room.State.VISIBLE)
