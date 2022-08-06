e8d6aaf0f0f68180ba091bc7f4a0a1f2a75f9b73
Rormattio <aurore.mattio@laposte.net>
2022-08-06 23:21:24 +0200

    wip total score

--- scenes/dining_room/Patron.gd
+++ scenes/dining_room/Patron.gd
@@ -256,7 +256,8 @@ func _on_EatTimer_timeout():
 	dish_score_value = res[0]
 	ingredient_diffs = res[1]
 	Global.patron_send_dish_score(received_dish.dish.serialize(), dish_score_value, received_dish.order.serialize(), ingredient_diffs)
-
+	Global.send_score(dish_score)
+	
 	command_avatar.rotation = 0
 	received_dish.queue_free()
 	set_state(State.SHOW_DISH_SCORE)
